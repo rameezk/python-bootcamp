be7641ed3966e8cc2fa4c7c4439a415e1a5cffcc
Rameez Khan <rameezkhan.sa@gmail.com>
2018-10-29 12:28:35 +0200

    Practice inheritance and polymorphism

---
+++ object-orientation/inheritance.py
@@ -0,0 +1,29 @@
+class Animal():
+    def __init__(self):
+        print("Creating animal")
+
+    def speak(self):
+        print("Generic sound")
+
+    def what_am_i(self):
+        print("I am an animal")
+
+class Dog(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+
+    def speak(self):
+        print("Woof")
+
+class Cat(Animal):
+    def __init__(self):
+        Animal.__init__(self)
+
+dog = Dog()
+dog.what_am_i()
+dog.speak()
+
+cat = Cat()
+cat.what_am_i()
+cat.speak()
+
